2159065fed581426ecf3a3041d9dcea632d85549
annabelle <annabelleaiza@gmail.com>
2026-05-09 12:43:00 -0400

    create Constants class

--- src/main/java/org/annabelle/domain/Constants.java
+++ src/main/java/org/annabelle/domain/Constants.java
@@ -1,6 +1,6 @@
 package org.annabelle.domain;
 
 public class Constants {
-    public static final String BOOKS_CSV_PATH = "";
+    public static final String ITEM_CSV_PATH = "";
     public static final String USER_CSV_PATH = "";
 }

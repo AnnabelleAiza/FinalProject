d7382e555a79c01901bc2deac0375abab05257c2
annabelle <annabelleaiza@gmail.com>
2026-05-09 09:01:07 -0400

    git.push

--- src/main/java/org/annabelle/domain/Admin.java
+++ src/main/java/org/annabelle/domain/Admin.java
@@ -1,5 +1,15 @@
 package org.annabelle.domain;
 
 public class Admin extends User implements Reportable {
+    public static void generateReport(Library library) {
 
+    }
+
+    public static void backupUsers() {
+
+    }
+
+    public static void backupItems() {
+
+    }
 }
--- src/main/java/org/annabelle/domain/Library.java
+++ src/main/java/org/annabelle/domain/Library.java
@@ -1,8 +1,39 @@
 package org.annabelle.domain;
 
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class Library {
-    public List<Item> items;
-    public List<User> users;
+    public Map<String, Item> items;
+    public Map<String, User> users;
+
+    public Library() {
+        this.items = new TreeMap<>();
+        this.users = new TreeMap<>();
+    }
+
+    public void borrowItem(String userId, String itemId) {
+
+    }
+
+    public void returnItem(String userId, String itemId) {
+
+    }
+
+    public List<Item> searchByTitle(String title) {
+
+    }
+
+    public List<Item> searchByAuthor(String Author) {
+
+    }
+
+    public Item searchRecursive(){
+
+    }
+
+    public List<Item> searchStream() {
+
+    }
 }

1d91c250fde26f036a45367ba84e188e3b51efa9
annabelle <annabelleaiza@gmail.com>
2026-05-09 12:58:20 -0400

    update implementations

--- src/main/java/org/annabelle/domain/Constants.java
+++ src/main/java/org/annabelle/domain/Constants.java
@@ -1,6 +1,6 @@
 package org.annabelle.domain;
 
 public class Constants {
-    public static final String ITEM_CSV_PATH = "";
-    public static final String USER_CSV_PATH = "";
+    public static final String ITEM_CSV_PATH = "src/main/resources/item.csv";
+    public static final String USER_CSV_PATH = "src/main/resources/user.csv";
 }
--- src/main/java/org/annabelle/domain/Reportable.java
+++ src/main/java/org/annabelle/domain/Reportable.java
@@ -1,5 +1,9 @@
 package org.annabelle.domain;
 
 public interface Reportable {
+    /**
+     * Generates a full report of the system
+     * @return report as a String
+     */
     String generateReport();
 }

08195d0afa949943dd74169f9315563deb6b77e9
annabelle <annabelleaiza@gmail.com>
2026-05-09 10:33:58 -0400

    update student class

--- src/main/java/org/annabelle/domain/Student.java
+++ src/main/java/org/annabelle/domain/Student.java
@@ -1,6 +1,10 @@
 package org.annabelle.domain;
 
 public class Student extends User {
+    public Student(String id, String name) {
+        super(id, name);
+    }
+
     @Override
     public boolean canBorrow(Item item) {
         return false;

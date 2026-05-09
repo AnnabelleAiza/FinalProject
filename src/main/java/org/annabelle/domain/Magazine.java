bf67bd2841c9901464cbafc7e7626ef96c6862f5
annabelle <annabelleaiza@gmail.com>
2026-05-09 08:51:47 -0400

    git.push

--- src/main/java/org/annabelle/domain/Magazine.java
+++ src/main/java/org/annabelle/domain/Magazine.java
@@ -1,6 +1,17 @@
 package org.annabelle.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Magazine extends Item {
     private String issueNumber;
     private String publisher;
+
+    public Magazine(String id, String title, String issueNumber, String publisher) {
+        super(id, title);
+        this.issueNumber = issueNumber;
+        this.publisher = publisher;
+    }
 }
--- src/main/java/org/annabelle/domain/User.java
+++ src/main/java/org/annabelle/domain/User.java
@@ -1,5 +1,6 @@
 package org.annabelle.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public abstract class User {
@@ -7,4 +8,21 @@ public abstract class User {
     protected String name;
     protected List<Item> borrowedItems;
 
+    public User(String id, String name) {
+        this.id = id;
+        this.name = name;
+        this.borrowedItems = new ArrayList<>();
+    }
+
+    public static void borrowItem(Item item) {
+
+    }
+
+    public static void returnItem(Item item) {
+
+    }
+
+    public abstract boolean canBorrow(Item item) {
+
+    }
 }
